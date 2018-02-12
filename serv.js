f9ef936fe15fd57b6bd9c0e2082eee1a10b32294
GitArcher <archinux01@gmail.com>
2018-02-12 19:54:53 +0200

    neuro2

--- serv.js
+++ serv.js
@@ -4,12 +4,11 @@ const http = require('http');
 const server = http.createServer(app);
 
 let port = process.env.PORT || 8080;
-let host = process.env.HOST || '192.168.1.101';
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname+'/index.html');
 });
 
-server.listen(port, host, () => {
+server.listen(port, () => {
   console.log(port, host);
 });
